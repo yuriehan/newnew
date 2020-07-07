3878e577dde527631d3eacd243ed54a25e0e186b
yuriehan <67681338+yuriehan@users.noreply.github.com>
2020-07-07 01:50:22 -0700

    Initialize tutorial-part-three

---
+++ gatsby-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'post',
+        path: `${__dirname}/blog`,
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-external-links',
+            options: {
+              target: '_blank',
+              rel: 'nofollow noopener noreferrer',
+            },
+          },
+          'gatsby-remark-autolink-headers',
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography.js',
+      },
+    },
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-offline',
+  ],
+};
---
+++ src/templates/post.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import Link from 'gatsby-link';
+import styled from 'styled-components';
+import { darken, lighten } from 'polished';
+import Wrapper from '../components/Wrapper';
+import Subline from '../components/Subline';
+import { media } from '../utils/media';
+
+const Content = styled.article`
+  grid-column: 2;
+  box-shadow: 0 4px 120px rgba(0, 0, 0, 0.1);
+  border-radius: 1rem;
+  padding: 2rem 4rem;
+  background-color: ${props => props.theme.bg};
+  z-index: 9000;
+  margin-top: -3rem;
+  @media ${media.tablet} {
+    padding: 3rem 3rem;
+  }
+  @media ${media.phone} {
+    padding: 2rem 1.5rem;
+  }
+`;
+
+const Header = styled.header`
+  background: linear-gradient(
+    45deg,
+    ${props => darken(0.1, props.theme.primary)},
+    ${props => lighten(0.1, props.theme.primary)}
+  );
+  grid-column: 1 / -1;
+  margin-left: -1rem;
+  margin-right: -1rem;
+  padding: 2rem 2rem 5rem 2rem;
+  box-shadow: inset 0px -10px 30px 0px rgba(0, 0, 0, 0.1);
+`;
+
+const HeaderContent = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  a {
+    color: white;
+  }
+`;
+
+const Title = styled.h1`
+  margin-top: 2rem;
+`;
+
+const PostContent = styled.div`
+  margin-top: 4rem;
+`;
+
+const Post = props => {
+  const postNode = props.data.markdownRemark;
+  const post = postNode.frontmatter;
+
+  return (
+    <Wrapper>
+      <Helmet title={`${post.title} | Gatsby Starter - Minimal Blog`} />
+      <Header>
+        <HeaderContent>
+          <Link to="/">Gatsby Starter - Minimal Blog</Link>
+        </HeaderContent>
+      </Header>
+      <Content>
+        <Title>{post.title}</Title>
+        <Subline>
+          {post.date} &mdash; {postNode.timeToRead} Min Read
+        </Subline>
+        <PostContent dangerouslySetInnerHTML={{ __html: postNode.html }} />
+      </Content>
+    </Wrapper>
+  );
+};
+
+export default Post;
+
+/* eslint no-undef: off */
+export const postQuery = graphql`
+  query postBySlug($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        date(formatString: "DD.MM.YYYY")
+      }
+      timeToRead
+    }
+  }
+`;
